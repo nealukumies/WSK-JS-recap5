d00bb19bfd2553034006de86442c2f9af4fc9e99
nealukumies <nea.lukumies@metropolia.fi>
2025-03-26 10:42:18 +0200

    task2

--- Task2/task2.js
+++ Task2/task2.js
@@ -1,6 +1,22 @@
-function greeting ()
-{
-  console.log("Hello World!");
-};
+async function postMethodFunction() {
+  const data = {
+    body: JSON.stringify({
+      first_name: 'Jane',
+      last_name: 'Sunny',
+    }),
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+  try {
+    const response = await fetch('https://reqres.in/api/users', data);
+    if (!response.ok) throw new Error('Invalid input!');
+    const json = await response.json();
+    console.log('result', json);
+  } catch (e) {
+    console.log('error', e);
+  }
+}
 
-greeting();
+postMethodFunction();
