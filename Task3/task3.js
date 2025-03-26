31acf9ac2d1f05bb3511a1f6a3e64d72d417ee6d
nealukumies <nea.lukumies@metropolia.fi>
2025-03-26 10:52:25 +0200

    task3

--- Task3/task3.js
+++ Task3/task3.js
@@ -1,6 +1,69 @@
-function greeting ()
-{
-  console.log("Hello World!");
-};
+async function getFunction() {
+  console.log('asynchronous download begins');
+  try {
+    const response = await fetch('https://reqres.in/api/unknown/23');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const jsonData = await response.json();
+    console.log(jsonData);
+  } catch (error) {
+    console.log('GET error: ' + error.message);
+  } finally {
+    console.log('asynchronous load complete');
+  }
+}
 
-greeting();
+async function postFunction() {
+  const data = {
+    body: JSON.stringify({
+      first_name: 'Jane',
+      last_name: 'Sunny',
+    }),
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+  try {
+    const response = await fetch('https://reqres.in/unknown/23', data);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const json = await response.json();
+    console.log('result', json);
+  } catch (e) {
+    if (e instanceof TypeError) {
+      console.log('Network error or invalid URL:', e.message);
+    } else {
+      console.log('POST error: ', e);
+    }
+  }
+}
+
+async function deleteFunction() {
+  const data = {
+    body: JSON.stringify({
+      first_name: 'Jane',
+      last_name: 'Sunny',
+    }),
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+  try {
+    const response = await fetch('https://reqres.in/unknown/23', data);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const json = await response.json();
+    console.log('result', json);
+  } catch (e) {
+    if (e instanceof TypeError) {
+      console.log('Network error or invalid URL:', e.message);
+    } else {
+      console.log('DELETE error: ', e);
+    }
+  }
+}
+
+getFunction();
+postFunction();
+deleteFunction();
