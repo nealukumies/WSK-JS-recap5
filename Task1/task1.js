d901c39a39774f812f3509027b1a797ea513e7e9
nealukumies <nea.lukumies@metropolia.fi>
2025-03-26 10:36:37 +0200

    task 1

--- Task1/task1.js
+++ Task1/task1.js
@@ -1,6 +1,14 @@
-function greeting ()
-{
-  console.log("Hello World!");
-};
+async function getMethdodFunction() {
+  console.log('asynchronous download begins');
+  try {
+    const response = await fetch('https://reqres.in/api/users/1');
+    const jsonData = await response.json();
+    console.log(jsonData);
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    console.log('asynchronous load complete');
+  }
+}
 
-greeting();
+getMethdodFunction();
