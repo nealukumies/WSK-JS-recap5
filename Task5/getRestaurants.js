b758fc3cc362e9fa0b8b34d219e45325ad7c6f19
nealukumies <nea.lukumies@metropolia.fi>
2025-03-26 11:11:51 +0200

    restaurants from API

---
+++ Task5/getRestaurants.js
@@ -0,0 +1,15 @@
+export async function getRestaurants() {
+  console.log('asynchronous download begins');
+  try {
+    const response = await fetch(
+      'https://media2.edu.metropolia.fi/restaurant/api/v1/restaurants'
+    );
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData;
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    console.log('asynchronous load complete');
+  }
+}
--- Task5/task5.js
+++ Task5/task5.js
@@ -1,6 +1,60 @@
-function greeting ()
-{
-  console.log("Hello World!");
-};
+import {getRestaurants} from './getRestaurants.js';
 
-greeting();
+async function init() {
+  restaurants = await getRestaurants();
+  restaurants.sort((a, b) => a.name.localeCompare(b.name));
+  renderTable();
+}
+
+function renderTable() {
+  const table = document.querySelector('table');
+  const dialog = document.querySelector('dialog');
+  for (const restaurant of restaurants) {
+    const tr = document.createElement('tr');
+    tr.addEventListener('click', function () {
+      tr.classList.add('highlight');
+      dialog.innerHTML = '';
+      dialog.showModal();
+      const h3 = document.createElement('h3');
+      h3.innerHTML = restaurant.name;
+      const p = document.createElement('p');
+      p.innerHTML = restaurant.address;
+      const p2 = document.createElement('p');
+      p2.innerHTML = restaurant.postalCode;
+      const p3 = document.createElement('p');
+      p3.innerHTML = restaurant.city;
+      const p4 = document.createElement('p');
+      p4.innerHTML = restaurant.phone;
+      const p5 = document.createElement('p');
+      p5.innerHTML = restaurant.company;
+      const closeButton = document.createElement('button');
+      closeButton.setAttribute('class', 'close-btn');
+      closeButton.innerHTML = 'Close';
+      closeButton.addEventListener('click', () => {
+        dialog.close();
+      });
+      dialog.append(h3, p, p2, p3, p4, p5, closeButton);
+    });
+
+    dialog.addEventListener('close', () => {
+      for (const elem of document.querySelectorAll('.highlight')) {
+        elem.classList.remove('highlight');
+      }
+    });
+
+    const nametd = document.createElement('td');
+    tr.appendChild(nametd);
+    nametd.innerHTML = restaurant.name;
+    const addresstd = document.createElement('td');
+    addresstd.innerHTML = restaurant.address;
+    tr.appendChild(addresstd);
+
+    const citytd = document.createElement('td');
+    citytd.innerHTML = restaurant.city;
+    tr.appendChild(citytd);
+    table.appendChild(tr);
+  }
+}
+
+let restaurants;
+init();
